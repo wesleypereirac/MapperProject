22a17e836e168cc115ed3b0f9f2d3c6a3a02a3c3
wesley <wesleyppereira12@gmail.com>
2026-03-29 17:15:48 -0300

    reestruturação, isolando as funções do codigo

--- mouse.py
+++ mouse.py
@@ -8,9 +8,40 @@ class ScriptManager:
 class Actions:
     is_aimming = False
     alt_holding = False
+    
+    #criar aq metodo referente à configuração? ex: ativar/desativasr funções, log e/ou função q muda sensi do touch
+    
+    #controla disparos de armas
+    def hold_alt():
+         if not Actions.alt_holding:
+                
+                #primeiro clica dps segura, pois unico clique nao atira
+                pyautogui.click(button='left')
+                pyautogui.mouseDown(button='left')  
+                log('click + segurar btn esquerdo')
+                Actions.alt_holding = True
+                
+    def release_alt():
+        if Actions.alt_holding:
+            
+            pyautogui.mouseUp(button='left')
+            log('soltando btn esquerdo')
+            Actions.alt_holding = False
+            
+    def switch_aim():
+        #verifica se é control, e alterna btn direito do mouse
+        if Actions.is_aimming:
+                pyautogui.mouseUp(button='right')
+                log('soltando btn direito')
+                Actions.is_aimming = False
+                
+        else:
+            pyautogui.mouseDown(button='right')
+            log('segurando btn direito')
+            Actions.is_aimming = True
+
 
 def log(msg, type='Log'):
-    
     
     if Actions.logs_on:
         compl = None
@@ -27,48 +58,25 @@ def log(msg, type='Log'):
 
 def on_press(key):
     is_script_paused = ScriptManager.is_script_paused 
-    is_aimming = Actions.is_aimming
-    alt_holding = Actions.alt_holding   
-
+    
     if key == keyboard.Key.delete:
-        
         
         is_script_paused = False if is_script_paused else True
         log(f'Teclado pausado: {is_script_paused}')
 
     if not is_script_paused:
-        #verifica se é control
         if key in (keyboard.Key.ctrl, keyboard.Key.ctrl_l, keyboard.Key.ctrl_r):
-            if is_aimming:
-                pyautogui.mouseUp(button='right')
-                log('soltando btn direito')
-                is_aimming = False
-                
-            else:
-                pyautogui.mouseDown(button='right')
-                log('segurando btn direito')
-                is_aimming = True
+            Actions.switch_aim()
 
-        #controla disparos de armas
         elif key in (keyboard.Key.alt, keyboard.Key.alt_l, keyboard.Key.alt_r):
-            if not alt_holding:
-                
-                #primeiro clica dps segura, pois unico clique nao atira
-                pyautogui.click(button='left')
-                pyautogui.mouseDown(button='left')  
-                log('click + segurar btn esquerdo')
-                alt_holding = True
+           Actions.hold_alt()
+
 
 #falta add a condição de is_script_paused
 def on_release(key):
-    Actions.alt_holding
-
-    if key in (keyboard.Key.alt, keyboard.Key.alt_l, keyboard.Key.alt_r):
-        if alt_holding:
-            
-            pyautogui.mouseUp(button='left')
-            log('soltando btn esquerdo')
-            alt_holding = False
+    if not ScriptManager.is_script_paused:
+        if key in (keyboard.Key.alt, keyboard.Key.alt_l, keyboard.Key.alt_r):
+            Actions.release_alt()
 
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
