95e9f5543efe89634add5c7c9d9c2cfc690dd638
wesley <wesleyppereira12@gmail.com>
2026-03-31 16:06:51 -0300

    refatorações e criação do trecho que alterna a sensi do mouse

--- mouse.py
+++ mouse.py
@@ -1,15 +1,66 @@
 import pyautogui
 from pynput import keyboard
+import subprocess, time
 
 class ScriptManager:
     is_script_paused = False
     logs_on = False
+    is_mouse_sensi_medium = True
     
     def pause_keyboard_listener():
         is_script_paused = ScriptManager.is_script_paused 
         ScriptManager.is_script_paused  = False if is_script_paused else True
         log(f'Teclado pausado: {is_script_paused}')
-
+        
+    def manage_config(mode='start'):
+        if mode == 'start':
+            subprocess.run(['start', 'ms-settings:'], shell=True)
+            ScriptManager.is_cfg_running = True
+        
+        else:
+            subprocess.call(["taskkill", "/IM", "SystemSettings.exe", "/F"])
+            ScriptManager.is_cfg_running = False
+        
+    def change_mosuse_sensi():
+        ScriptManager.manage_config('start')
+        
+        time.sleep(2)
+        
+        #alterar sensi
+        pyautogui.click(583,27)
+        
+        ##search app
+        word = 'touch'
+        for i in word:
+            pyautogui.press(i)
+        
+        time.sleep(0.5)
+        pyautogui.press('Enter', presses=3)
+        
+        ##alterar sensi
+        time.sleep(1)
+        pyautogui.click(805,401)
+        
+        time.sleep(1)
+        
+        pyautogui.click(1194,461)
+        
+        time.sleep(1)
+        
+        if ScriptManager.is_mouse_sensi_medium == True:
+            pyautogui.press('up',presses=3)
+        else:
+            pyautogui.press('down',presses=2)
+            
+        time.sleep(1)
+        pyautogui.press('enter')
+        
+        
+        ###para orientar sobre o estado
+        bool = False if ScriptManager.is_mouse_sensi_medium == True else True
+        
+        ScriptManager.is_mouse_sensi_medium = bool
+        
 
 class Actions:
     is_aimming = False
@@ -66,7 +117,9 @@ def on_press(key):
   
     if key == keyboard.Key.delete:
        ScriptManager.pause_keyboard_listener()
-       
+    if key == keyboard.Key.f3:
+        return False
+    
     if not ScriptManager.is_script_paused:
         if key in (keyboard.Key.ctrl, keyboard.Key.ctrl_l, keyboard.Key.ctrl_r):
             Actions.switch_aim()
@@ -80,6 +133,9 @@ def on_release(key):
         if key in (keyboard.Key.alt, keyboard.Key.alt_l, keyboard.Key.alt_r):
             Actions.release_alt()
 
+ScriptManager.change_mosuse_sensi()
+time.sleep(3)
+ScriptManager.change_mosuse_sensi()
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     log('Running', 'status')
